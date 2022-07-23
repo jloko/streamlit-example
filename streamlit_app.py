9ff42b8e16d6a49f12c803d1d0cec1836df022b5
jloko <39775404+jloko@users.noreply.github.com>
2022-07-23 22:19:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,23 +16,64 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+import pandas as pd
+import streamlit as st
+import zipfile
+import base64
+import os
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# Web App Title
+st.markdown('''
+# **Excel File Merger**
+Josh - This is the **Excel File Merger App** created in Python using the Streamlit library.
+**Credit:** App built in `Python` + `Streamlit` by [Chanin Nantasenamat](https://medium.com/@chanin.nantasenamat) (aka [Data Professor](http://youtube.com/dataprofessor))
+---
+''')
 
-    points_per_turn = total_points / num_turns
+# Excel file merge function
+def excel_file_merge(zip_file_name):
+    df = pd.DataFrame()
+    archive = zipfile.ZipFile(zip_file_name, 'r')
+    with zipfile.ZipFile(zip_file_name, "r") as f:
+        for file in f.namelist():
+          xlfile = archive.open(file)
+          if file.endswith('.xlsx'):
+            # Add a note indicating the file name that this dataframe originates from
+            df_xl = pd.read_excel(xlfile)
+            df_xl['Note'] = file
+            # Appends content of each Excel file iteratively
+            df = df.append(df_xl, ignore_index=True)
+    return df
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# Upload CSV data
+with st.sidebar.header('1. Upload your ZIP file'):
+    uploaded_file = st.sidebar.file_uploader("Excel-containing ZIP file", type=["zip"])
+    st.sidebar.markdown("""
+[Example ZIP input file](https://github.com/dataprofessor/excel-file-merge-app/raw/main/nba_data.zip)
+""")
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# File download
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="merged_file.csv">Download Merged File as CSV</a>'
+    return href
+
+def xldownload(df):
+    df.to_excel('data.xlsx', index=False)
+    data = open('data.xlsx', 'rb').read()
+    b64 = base64.b64encode(data).decode('UTF-8')
+    #b64 = base64.b64encode(xl.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/xls;base64,{b64}" download="merged_file.xlsx">Download Merged File as XLSX</a>'
+    return href
+
+# Main panel
+if st.sidebar.button('Submit'):
+    #@st.cache
+    df = excel_file_merge(uploaded_file)
+    st.header('**Merged data**')
+    st.write(df)
+    st.markdown(filedownload(df), unsafe_allow_html=True)
+    st.markdown(xldownload(df), unsafe_allow_html=True)
+else:
+    st.info('Awaiting for ZIP file to be uploaded.')
